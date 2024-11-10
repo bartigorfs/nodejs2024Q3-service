e36d871967f8ff1e8eb6868eb8c1970f0d1fb08a
Igor Bartashevich <86968311+bartigorfs@users.noreply.github.com>
2024-11-10 22:49:48 +0300

    feat: add validation pipe;

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
-import { NestFactory } from "@nestjs/core";
-import { AppModule, getEnvPath } from "./app.module";
-import { INestApplication } from "@nestjs/common";
+import { NestFactory } from '@nestjs/core';
+import { AppModule, getEnvPath } from './app.module';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap(): Promise<void> {
   const app: INestApplication = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(AppModule.port);
 }
